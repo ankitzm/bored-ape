4a71ad52323a86c9f2e794fe50aada133891fa20
Ankit Singh <ankit.singh.zm@gmail.com>
2022-07-18 12:34:07 +0530

    updated queries and fixed if statement for !token

--- generated/schema.ts
+++ generated/schema.ts
@@ -69,49 +69,124 @@ export class Token extends Entity {
     this.set("image", Value.fromString(value));
   }
 
-  get background(): string {
+  get createdAtTimestamp(): BigInt {
+    let value = this.get("createdAtTimestamp");
+    return value!.toBigInt();
+  }
+
+  set createdAtTimestamp(value: BigInt) {
+    this.set("createdAtTimestamp", Value.fromBigInt(value));
+  }
+
+  get updatedAtTimestamp(): BigInt | null {
+    let value = this.get("updatedAtTimestamp");
+    if (!value || value.kind == ValueKind.NULL) {
+      return null;
+    } else {
+      return value.toBigInt();
+    }
+  }
+
+  set updatedAtTimestamp(value: BigInt | null) {
+    if (!value) {
+      this.unset("updatedAtTimestamp");
+    } else {
+      this.set("updatedAtTimestamp", Value.fromBigInt(<BigInt>value));
+    }
+  }
+
+  get background(): string | null {
     let value = this.get("background");
-    return value!.toString();
+    if (!value || value.kind == ValueKind.NULL) {
+      return null;
+    } else {
+      return value.toString();
+    }
   }
 
-  set background(value: string) {
-    this.set("background", Value.fromString(value));
+  set background(value: string | null) {
+    if (!value) {
+      this.unset("background");
+    } else {
+      this.set("background", Value.fromString(<string>value));
+    }
   }
 
-  get fur(): string {
+  get fur(): string | null {
     let value = this.get("fur");
-    return value!.toString();
+    if (!value || value.kind == ValueKind.NULL) {
+      return null;
+    } else {
+      return value.toString();
+    }
   }
 
-  set fur(value: string) {
-    this.set("fur", Value.fromString(value));
+  set fur(value: string | null) {
+    if (!value) {
+      this.unset("fur");
+    } else {
+      this.set("fur", Value.fromString(<string>value));
+    }
   }
 
-  get hat(): string {
+  get hat(): string | null {
     let value = this.get("hat");
-    return value!.toString();
+    if (!value || value.kind == ValueKind.NULL) {
+      return null;
+    } else {
+      return value.toString();
+    }
   }
 
-  set hat(value: string) {
-    this.set("hat", Value.fromString(value));
+  set hat(value: string | null) {
+    if (!value) {
+      this.unset("hat");
+    } else {
+      this.set("hat", Value.fromString(<string>value));
+    }
   }
 
-  get mouth(): string {
+  get mouth(): string | null {
     let value = this.get("mouth");
-    return value!.toString();
+    if (!value || value.kind == ValueKind.NULL) {
+      return null;
+    } else {
+      return value.toString();
+    }
   }
 
-  set mouth(value: string) {
-    this.set("mouth", Value.fromString(value));
+  set mouth(value: string | null) {
+    if (!value) {
+      this.unset("mouth");
+    } else {
+      this.set("mouth", Value.fromString(<string>value));
+    }
   }
 
-  get eyes(): string {
+  get eyes(): string | null {
     let value = this.get("eyes");
+    if (!value || value.kind == ValueKind.NULL) {
+      return null;
+    } else {
+      return value.toString();
+    }
+  }
+
+  set eyes(value: string | null) {
+    if (!value) {
+      this.unset("eyes");
+    } else {
+      this.set("eyes", Value.fromString(<string>value));
+    }
+  }
+
+  get creator(): string {
+    let value = this.get("creator");
     return value!.toString();
   }
 
-  set eyes(value: string) {
-    this.set("eyes", Value.fromString(value));
+  set creator(value: string) {
+    this.set("creator", Value.fromString(value));
   }
 
   get owner(): string {
@@ -162,5 +237,14 @@ export class User extends Entity {
 
   set tokens(value: Array<string>) {
     this.set("tokens", Value.fromStringArray(value));
+  }
+
+  get created(): Array<string> {
+    let value = this.get("created");
+    return value!.toStringArray();
+  }
+
+  set created(value: Array<string>) {
+    this.set("created", Value.fromStringArray(value));
   }
 }
